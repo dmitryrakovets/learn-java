d1c1b76e81116cca3888cd2a883dcdeaf84824f6
Dmitry Rakovets <dmitryrakovets@yandex.by>
2023-01-26 22:32:02 +0300

    feat: add solution for Task 01

---
+++ src/Task01.java
@@ -0,0 +1,12 @@
+public class Task01 {
+    public static void main(String[] args) {
+        int a = 7;
+        int b = 9;
+
+        int c = a / b;
+        int d = a * b;
+
+        System.out.println(c);
+        System.out.println(d);
+    }
+}
